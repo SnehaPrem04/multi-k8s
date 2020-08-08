ddb2344da7253d9231f9d1254f1ba14a108eef54
SnehaPrem04 <snehaprem04@yahoo.com>
2020-08-08 15:32:15 +0530

    encrypt addition

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
- docker build -t snehaprem/multi-client -t snehaprem/multi-client:$SHA -f ./client/Dockerfile ./client
- docker build -t snehaprem/multi-server -t snehaprem/multi-server:$SHA -f ./server/Dockerfile ./server
- docker build -t snehaprem/multi-worker -t snehaprem/multi-worker:$SHA -f ./worker/Dockerfile ./worker
- docker push snehaprem/multi-client
- docker push snehaprem/multi-server
- docker push snehaprem/multi-worker
- docker push snehaprem/multi-client:$SHA
- docker push snehaprem/multi-server:$SHA
- docker push snehaprem/multi-worker:$SHA
- kubectl apply -f k8s
- kubectl set image deployment/server-deployment server=snehaprem/multi-server:$SHA
- kubectl set image deployment/worker-deployment server=snehaprem/multi-worker:$SHA
- kubectl set image deployment/client-deployment server=snehaprem/multi-client:$SHA
+docker build -t snehaprem/multi-client:latest -f ./client/Dockerfile ./client
+docker build -t snehaprem/multi-server:latest -f ./server/Dockerfile ./server
+docker build -t snehaprem/multi-worker:latest -f ./worker/Dockerfile ./worker
+docker build -t snehaprem/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t snehaprem/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t snehaprem/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push snehaprem/multi-client:latest
+docker push snehaprem/multi-server:latest
+docker push snehaprem/multi-worker:latest
+docker push snehaprem/multi-client:$SHA
+docker push snehaprem/multi-server:$SHA
+docker push snehaprem/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployment/server-deployment server=snehaprem/multi-server:$SHA
+kubectl set image deployment/worker-deployment server=snehaprem/multi-worker:$SHA
+kubectl set image deployment/client-deployment server=snehaprem/multi-client:$SHA
