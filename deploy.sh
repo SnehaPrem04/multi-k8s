67a07ac654a30f3d634d35c0d3edb00c0de6e790
SnehaPrem04 <snehaprem04@yahoo.com>
2020-08-08 14:51:59 +0530

    deploy,travis

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
- docker build -t snehaprem/multi-client:latest -t snehaprem/multi-client:$SHA -f ./client/Dockerfile ./client
- docker build -t snehaprem/multi-server:latest -t snehaprem/multi-server:$SHA -f ./server/Dockerfile ./server
- docker build -t snehaprem/multi-worker:latest -t snehaprem/multi-worker:$SHA -f ./worker/Dockerfile ./worker
- docker push snehaprem/multi-client:latest
- docker push snehaprem/multi-server:latest
- docker push snehaprem/multi-worker:latest
- docker push snehaprem/multi-client:$SHA
- docker push snehaprem/multi-server:$SHA
- docker push snehaprem/multi-worker:$SHA
+ docker build -t snehaprem04/multi-client:latest -t snehaprem04/multi-client:$SHA -f ./client/Dockerfile ./client
+ docker build -t snehaprem04/multi-server:latest -t snehaprem04/multi-server:$SHA -f ./server/Dockerfile ./server
+ docker build -t snehaprem04/multi-worker:latest -t snehaprem04/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+ docker push snehaprem04/multi-client:latest
+ docker push snehaprem04/multi-server:latest
+ docker push snehaprem04/multi-worker:latest
+ docker push snehaprem04/multi-client:$SHA
+ docker push snehaprem04/multi-server:$SHA
+ docker push snehaprem04/multi-worker:$SHA
  kubectl apply -f k8s
- kubectl set image deployment/server-deployment server=snehaprem/multi-server:$SHA
- kubectl set image deployment/worker-deployment server=snehaprem/multi-worker:$SHA
- kubectl set image deployment/client-deployment server=snehaprem/multi-client:$SHA
+ kubectl set image deployment/server-deployment server=snehaprem04/multi-server:$SHA
+ kubectl set image deployment/worker-deployment server=snehaprem04/multi-worker:$SHA
+ kubectl set image deployment/client-deployment server=snehaprem04/multi-client:$SHA
